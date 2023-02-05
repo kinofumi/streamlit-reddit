1c6e514d70293868b7a048dd15d82ce8cc956ab4
Kinoshita Fumichika <fumichika@under-the-tree.jp>
2023-02-05 13:00:11 +0900

    余分なコードの削除

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ from google.oauth2 import service_account
 import json
 
 key_dict = json.loads(st.secrets["textkey"])
-print(key_dict)
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="streamlit-reddit-4a96e")
 
